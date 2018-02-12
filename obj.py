f33941dc565774928f661be17bf2e76cd0b9c499
Burk0 <buransky.david@centrum.sk>
2018-02-12 18:54:38 +0100

    uprava

---
+++ obj.py
@@ -0,0 +1,8 @@
+
+
+class obj(object):
+    def __init__(self,id,title,year,content):
+        self.id=id
+        self.title=title
+        self.year=year
+        self.content=content
--- webServer.py
+++ webServer.py
@@ -2,10 +2,16 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import backend
-import cgi
+import json
+import cgi,cgitb
+
+
+cgitb.enable() #for debugging
 
 class testHTTPServer_RequestHanlder(BaseHTTPRequestHandler):
     Page = open('index.html')
+    # json_string = json.dumps(backend.getData())
+
     # Page = '''\
     # <html>
     # <body>
@@ -33,48 +39,60 @@ class testHTTPServer_RequestHanlder(BaseHTTPRequestHandler):
     #     file.close()
 
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        # page = self.create_page()
+        if self.path.endswith("/browse"):
+            self.send_page('index.html')
+        elif self.path.endswith("/cams"):
+            print("ina stranka")
+            self.send_page('addCamForm.html')
 
-    def create_page(self):
-        values = {
-            'date_time': self.date_time_string(),
-            'client_host': self.client_address[0],
-            'client_port': self.client_address[1],
-            'command': self.command,
-            'path': self.path,
-            'list': backend.getData(),
-            'lenght' : len(backend.getData())
-        }
-        page = self.Page.read().format(**values)
-        return page
+
 
     def send_page(self,page):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(len(page)))
+        # self.send_header("Content-Length", str(len(page)))
         self.end_headers()
-        self.wfile.write(bytes(page,"utf8"))
+        file = open(page)
+        self.wfile.write(bytes(file.read(),"utf8"))
+
+
 
 
     def do_POST(self):
-        self.send_response(200)
-        # self.send_response(301)
-        # self.send_header('Location','/snimka')
-        # self.end_headers()
-        form = cgi.FieldStorage(
-            fp=self.rfile,
-            headers=self.headers,
-            environ={'REQUEST_METHOD': 'POST'}
-        )
-        print ("lal:" +form.getvalue("foo"))
-        self.wfile.write(bytes("<html><body><h1>POST Request Received!</h1></body></html>","utf8"))
+        # print(self.path)
+        if self.path.endswith("/browse"):
+            print(self.path)
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            # print(self.json_string)
+            self.wfile.write(bytes(backend.getData(), "utf8"))
+        #     self.send_response(200)
+        elif "/detail/" in self.path:
+            print("tu som " + self.path[8:])
+            self.wfile.write(bytes(backend.getDetail(9), "utf8"))
+
+        elif "/addCam" in self.path:
+            print(self.path)
+            # content_length = int(self.headers['Content-Length'])
+            # print(self.rfile.read(content_length))
+            form = cgi.FieldStorage()
+
+            username = form.getvalue("username")
+
+            print(username)
+        else:
+            print(self.path)
+
+            # self.wfile.write(bytes("posielam novy json","utf8"))
+    #     self.wfile.write(bytes("<html><body><h1>POST Request Received!</h1></body></html>","utf8"))
 
 
 
 def run():
     print('starting server...')
-    server_address = ('127.0.0.1', 8081)
+    server_address = ('127.0.0.1', 8082)
 
     httpd = HTTPServer(server_address,testHTTPServer_RequestHanlder)
     print('running server...')
