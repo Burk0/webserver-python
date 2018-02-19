164df8f48f7e47e0d52c2b8edcaf01e95aa20db7
Burk0 <buransky.david@centrum.sk>
2018-02-19 23:44:51 +0100

    Vytvorene objekty, ktore vytahuju data z db Tieto objekty pridane do JSONu Upraveny index2.html Vytvoreny motion detektor

---
+++ kamera.py
@@ -0,0 +1,6 @@
+
+class Kamera(object):
+    def __init__(self,id,nazov,typ):
+        self.id=id
+        self.nazov=nazov
+        self.typ=typ
---
+++ view.py
@@ -0,0 +1,7 @@
+
+
+class View(object):
+    def __init__(self,id,nazov,kamera_meno):
+        self.id=id
+        self.nazov=nazov
+        self.kamera_meno=kamera_meno
