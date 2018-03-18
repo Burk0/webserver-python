2f94513c12e8d6c6190c6fcfe2213fb09558aaba
Burk0 <buransky.david@centrum.sk>
2018-03-18 23:24:59 +0100

    nova funkcionalita

---
+++ Model/JsonData.py
@@ -0,0 +1,7 @@
+
+
+class JsonData(object):
+    def __init__(self,list_cams,list_views,list_shots):
+        self.cams=list_cams
+        self.views=list_views
+        self.snimky=list_shots
---
+++ Model/JsonModel.py
@@ -0,0 +1,5 @@
+
+
+class JsonModel(object):
+    def __init__(self,list):
+        self.list=list
